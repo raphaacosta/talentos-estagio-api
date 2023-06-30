e4e52365c43d4ef9a124fdfadbac0eebd568275f
Raphael Costa <rafa.costa0@hotmail.com>
2023-06-30 14:29:19 -0300

    add cors and express functions to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("running")
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT || 3333);
